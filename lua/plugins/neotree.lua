7383e8d89b82109fe9ef5d27c14837c4818d3b73
lms21a <lukasdev928@outlook.com>
2024-12-14 21:20:03 -0600

    Added Displaying Hidden Files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,6 +9,15 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<C-b>', ":Neotree filesystem reveal left<CR>")
+    require('neo-tree').setup {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
+    }
+  }
+      vim.keymap.set('n', '<C-b>', ":Neotree filesystem reveal left<CR>")
   end
 }
