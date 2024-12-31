bc6e1776ae44ddd03e901c3062863ae74a272e4a
lms21a <lukasdev928@outlook.com>
2024-12-31 13:11:38 -0600

    adjusting tab size

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require('mason-lspconfig').setup({
-        ensure_installed = {"lua_ls", "ruff", "pyright"}
+        ensure_installed = {"lua_ls", "ruff", "pyright", "clangd"}
       })
     end
 
@@ -29,6 +29,12 @@ return {
       lspconfig.pyright.setup({
         capabilities = capabilities,
         filetypes = {"python"},
+      })
+      lspconfig.clangd.setup({
+        init_options = {
+          fallbackFlags = {'--std=c++20'}
+      },
+            capabilities = capabilities,
       })
 
       vim.keymap.set('n','K', vim.lsp.buf.hover, {})
--- lua/vim_options.lua
+++ lua/vim_options.lua
@@ -2,10 +2,10 @@
 vim.opt.number = true
 
 vim.g.mapleader = " "
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+vim.opt.expandtab = false 
 vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 local function send_tmux_command(command)
   vim.fn.system("tmux " .. command)
